42b560147fabb5ee62a2344a4fd6f6190d9ce300
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-21 23:44:09 +0100

    Add mock test components

---
+++ src/__Mock_test/rocketSlice.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseAPI = 'https://api.spacexdata.com/v4/rockets';
+export const fetchRocket = createAsyncThunk('rockets/fetchRocket', async () => {
+  try {
+    const response = await axios.get(baseAPI);
+    return response.data;
+  } catch (error) {
+    throw new Error('Something went wrong');
+  }
+});
+const initialState = {
+  rockets: [],
+  isLoading: false,
+  error: null,
+};
+
+const rocketSlice = createSlice({
+  name: 'rocket',
+  initialState,
+  reducers: {
+    reserveRocket: (state, action) => {
+      const rocketId = action.payload;
+      state.rockets = state.rockets.map((rocketItems) => {
+        if (rocketItems.id === rocketId) {
+          return { ...rocketItems, reserved: true };
+        }
+        return rocketItems;
+      });
+    },
+    reservedCancel: (state, action) => {
+      const rocketId = action.payload;
+      state.rockets = state.rockets.map((rocketItems) => {
+        if (rocketItems.id === rocketId) {
+          return { ...rocketItems, reserved: false };
+        }
+        return rocketItems;
+      });
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRocket.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(fetchRocket.fulfilled, (state, action) => {
+        state.rockets = action.payload.map((rocket) => ({
+          id: rocket.id,
+          rocket_name: rocket.name,
+          description: rocket.description,
+          flickr_images: rocket.flickr_images,
+          reserved: false,
+        }));
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(fetchRocket.rejected, (state) => {
+        state.isLoading = false;
+        state.error = true;
+      });
+  },
+});
+export const { reserveRocket, reservedCancel } = rocketSlice.actions;
+export default rocketSlice.reducer;
