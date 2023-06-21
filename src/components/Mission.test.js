a7a29ea921e33f532cf850fa13cd9ff3acd24807
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-22 00:41:50 +0300

    Fix linter errors

--- src/components/Mission.test.js
+++ src/components/Mission.test.js
@@ -1,9 +1,6 @@
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from '../Redux/store';
-import Mission from './Mission';
 import { useDispatch } from 'react-redux';
+import Mission from './Mission';
 
 // Mock useDispatch
 jest.mock('react-redux', () => ({
@@ -13,30 +10,28 @@ jest.mock('react-redux', () => ({
 describe('Mission Component', () => {
   const mockDispatch = jest.fn();
   useDispatch.mockReturnValue(mockDispatch);
-  
-  const details = {
-    id: '9D1B7E0',
-    name: 'Thaicom',
-    description: 'Thaicom is the name of a series of communications satellites operated from Thailand.',
-    reserved: false,
-  };
-  
+
   test('Render a mission', () => {
-    const { getByText } = render(<Mission {...details} />);
+    const { getByText } = render(<Mission
+      id="9D1B7E0"
+      name="Thaicom"
+      description="Thaicom is the name of a series of communications satellites operated from Thailand."
+      reserved={false}
+    />);
     expect(getByText('Thaicom')).toBeInTheDocument();
     expect(getByText('Thaicom is the name of a series of communications satellites operated from Thailand.')).toBeInTheDocument();
     expect(getByText('NOT A MEMBER')).toBeInTheDocument();
     expect(getByText('Join Mission')).toBeInTheDocument();
   });
-  
+
   test('Render active member', () => {
-    const reservedMission = {
-      ...details,
-      reserved: true,
-    };
-    const { getByText } = render(<Mission {...reservedMission} />);
+    const { getByText } = render(<Mission
+      id="9D1B7E0"
+      name="Thaicom"
+      description="Thaicom is the name of a series of communications satellites operated from Thailand."
+      reserved
+    />);
     expect(getByText('Active Member')).toBeInTheDocument();
     expect(getByText('Leave Mission')).toBeInTheDocument();
   });
-
 });
