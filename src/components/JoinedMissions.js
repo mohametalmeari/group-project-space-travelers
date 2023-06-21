6972ba06e99eafab8ba3ba4bcbd04fcc4029a1b8
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-21 20:26:44 +0300

    Add (No missions jioned) message

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -3,6 +3,16 @@ import styles from '../styles/missions.module.css';
 
 const JoinedMissions = () => {
   const { missions } = useSelector((state) => state.mission);
+  const noReserved = missions.every((item) => !item.reserved);
+  if (noReserved) {
+    return (
+      <div className={styles.joinedMissionsContainer}>
+        <h2 className={styles.joinedMissionsHeader}>My Missions</h2>
+        <p className={styles.noMissionsJoined}>No missions joined</p>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.joinedMissionsContainer}>
       <h2 className={styles.joinedMissionsHeader}>My Missions</h2>
