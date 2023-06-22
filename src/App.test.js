d059194868847747ea716fcb363b3603cb8e71e9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-22 12:18:13 +0100

    update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store'; // Import your Redux store
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+  );
+
+  const linkElement = screen.getByText(/Rockets/i);
   expect(linkElement).toBeInTheDocument();
 });
