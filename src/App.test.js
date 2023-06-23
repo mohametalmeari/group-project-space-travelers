c58f567a9f162e60290ab01dc19daae7fc0f3c78
MOHAMAD ALMEARI <mohamet.almeari@gmail.com>
2023-06-23 10:47:07 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 // import { MemoryRouter } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './Redux/store'; // Import your Redux store
 import App from './App';
 import store from './Redux/store';
 
